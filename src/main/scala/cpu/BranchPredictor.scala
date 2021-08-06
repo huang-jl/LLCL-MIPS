028990e54b7046120303630fd00d007c05e62de1
inaba_minoru <inaba_minoru@outlook.com>
2021-08-06 15:33:08 +0800

    修改 assignJump 信号，并格式化

--- src/main/scala/cpu/BranchPredictor.scala
+++ src/main/scala/cpu/BranchPredictor.scala
@@ -7,13 +7,13 @@ import lib._
 
 object Address {
   val btbIndexOffset = 3
-  val btbTagOffset = btbIndexOffset + BTB.INDEX_WIDTH
-  val btbTagWidth = 32 - btbTagOffset
+  val btbTagOffset   = btbIndexOffset + BTB.INDEX_WIDTH
+  val btbTagWidth    = 32 - btbTagOffset
 
   val btbMetaWidth = 2 + btbTagWidth
 
   val bhtIndexOffset = 3
-  val bhtBaseOffset = bhtIndexOffset + BHT.INDEX_WIDTH
+  val bhtBaseOffset  = bhtIndexOffset + BHT.INDEX_WIDTH
 
   val phtIndexOffset = 3
 
@@ -24,50 +24,46 @@ class Address extends UInt {
   setWidth(32)
 
   import Address._
-
-  def btbIndex: UInt = this (btbIndexOffset, BTB.INDEX_WIDTH bits)
-
-  def btbTag: UInt = this (btbTagOffset, btbTagWidth bits)
-
-  def bhtBase: UInt = this (bhtBaseOffset, BHT.INDEX_WIDTH bits)
-
-  def bhtIndex: UInt = this (bhtIndexOffset, BHT.INDEX_WIDTH bits)
-
-  def phtIndex: UInt = this (phtIndexOffset, PHT.INDEX_WIDTH bits)
+  def btbIndex: UInt = this(btbIndexOffset, BTB.INDEX_WIDTH bits)
+  def btbTag: UInt   = this(btbTagOffset, btbTagWidth bits)
+  def bhtBase: UInt  = this(bhtBaseOffset, BHT.INDEX_WIDTH bits)
+  def bhtIndex: UInt = this(bhtIndexOffset, BHT.INDEX_WIDTH bits)
+  def phtIndex: UInt = this(phtIndexOffset, PHT.INDEX_WIDTH bits)
 }
 
-class BranchPredictor(numStages: Int) extends Component {
-  val pc = Key(Address())
+class BranchPredictor(numStages: Int) extends Area {
+  val pc      = Key(Address())
   val isDJump = Key(Bool())
 
   val btbMetaLine = Key(Vec(Bits(Address.btbMetaWidth bits), BTB.NUM_WAYS))
   val btbDataLine = Key(Vec(Bits(30 bits), BTB.NUM_WAYS))
-  val btbHitLine = Key(Bits(BTB.NUM_WAYS bits))
-  val btbHit = Key(Bool())
-  val btbData = Key(UInt(30 bits))
-  val btbP = Key(Bits(BTB.NUM_WAYS - 1 bits))
+  val btbHitLine  = Key(Bits(BTB.NUM_WAYS bits))
+  val btbHit      = Key(Bool())
+  val btbData     = Key(UInt(30 bits))
+  val btbPC_2     = Key(Bool())
+  val btbP        = Key(Bits(BTB.NUM_WAYS - 1 bits))
 
-  val bhtI = Key(UInt(BHT.INDEX_WIDTH bits))
-  val bhtV = Key(Bits(BHT.DATA_WIDTH bits))
-  val phtI = Key(UInt(PHT.INDEX_WIDTH bits))
-  val phtV = Key(UInt(2 bits))
+  val bhtI  = Key(UInt(BHT.INDEX_WIDTH bits))
+  val bhtV  = Key(Bits(BHT.DATA_WIDTH bits))
+  val phtI  = Key(UInt(PHT.INDEX_WIDTH bits))
+  val phtV  = Key(UInt(2 bits))
   val phtVP = Key(UInt(2 bits))
   val phtVM = Key(UInt(2 bits))
 
   val io = new Bundle {
-    val pc = in(Address())
-    val nextPC = in(Address())
+    val pc     = Address()
+    val nextPC = Address()
 
-    val isDJump1 = in(Bool())
-    val jumpPC1 = in(UInt(32 bits))
+    val isDJump1 = Bool()
+    val jumpPC1  = UInt(32 bits)
 
-    val isDJump2 = in(Bool())
-    val jumpPC2 = in(UInt(32 bits))
+    val isDJump2 = Bool()
+    val jumpPC2  = UInt(32 bits)
 
-    val jump = in(Bool())
+    val jump = Bool()
 
-    val assignJump = out Bool()
-    val jumpPC = out UInt (32 bits)
+    val assignJump = Bits(2 bits)
+    val jumpPC     = UInt(32 bits)
   }
 
   val btb = new Cache(BTB.NUM_ENTRIES, BTB.NUM_WAYS, BTB.INDEX_WIDTH, Address.btbMetaWidth, 30)
@@ -84,15 +80,15 @@ class BranchPredictor(numStages: Int) extends Component {
     bpu.io.w.bhtEn := stored(isDJump)
     bpu.io.w.phtEn := stored(isDJump)
     bpu.io.w.bhtI := stored(bhtI)
-    bpu.io.w.bhtV := stored(bhtV)(BHT.DATA_WIDTH - 1 downto 1) ## io.jump
+    bpu.io.w.bhtV := stored(bhtV)(1, BHT.DATA_WIDTH - 1 bits) ## io.jump
     bpu.io.w.phtI := stored(phtI)
     bpu.io.w.phtV := io.jump ? stored(phtVP) | stored(phtVM)
   }
 
   val calc1 = new ComponentStage {
-    val pc_2 = io.isDJump2
+    val pc_2  = io.isDJump2
     val valid = io.isDJump1 | io.isDJump2
-    val we = stored(btbHit) ? stored(btbHitLine) | btb.plru(stored(btbP))
+    val we    = stored(btbHit) ? stored(btbHitLine) | btb.plru(stored(btbP))
     output(isDJump) := valid
     output(btbMetaLine) := stored(btbMetaLine)
     output(btbDataLine) := stored(btbDataLine)
@@ -108,7 +104,8 @@ class BranchPredictor(numStages: Int) extends Component {
     bpu.io.r.phtI := stored(phtI)
     output(phtV) := bpu.io.r.phtV
     /**/
-    io.assignJump := stored(btbHit) & output(phtV)(1)
+    val j = stored(btbHit) & output(phtV)(1)
+    io.assignJump := (j & stored(btbPC_2)) ## (j & !stored(btbPC_2))
     io.jumpPC := stored(btbData) @@ U"00"
   }
 
@@ -122,6 +119,7 @@ class BranchPredictor(numStages: Int) extends Component {
     }
     output(btbHit) := output(btbHitLine).orR
     output(btbData) := U(output(btbDataLine).oneHotAccess(output(btbHitLine)))
+    output(btbPC_2) := output(btbMetaLine).oneHotAccess(output(btbHitLine))(Address.btbMetaWidth - 1)
     /**/
     bpu.io.r.bhtI := io.pc.bhtBase ^ io.pc.bhtIndex
     output(bhtI) := bpu.io.r.bhtI
@@ -142,6 +140,8 @@ class BranchPredictor(numStages: Int) extends Component {
 
 object BranchPredictor {
   def main(args: Array[String]): Unit = {
-    SpinalVerilog(new BranchPredictor(5))
+    SpinalVerilog(new Component {
+      val bp = new BranchPredictor(5)
+    })
   }
 }
