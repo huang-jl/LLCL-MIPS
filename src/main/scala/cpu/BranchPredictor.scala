46ec1c6db69e3be7d781ce476e761ae49920612f
inaba_minoru <inaba_minoru@outlook.com>
2021-08-06 21:30:24 +0800

    将 BranchPredictor 拆成读写两段以支持指令队列

--- src/main/scala/cpu/BranchPredictor.scala
+++ src/main/scala/cpu/BranchPredictor.scala
@@ -31,17 +31,17 @@ class Address extends UInt {
   def phtIndex: UInt = this(phtIndexOffset, PHT.INDEX_WIDTH bits)
 }
 
-class BranchPredictor(numStages: Int) extends Area {
+class BranchPredictor extends Area {
   val pc      = Key(Address())
   val isDJump = Key(Bool())
 
-  val btbMetaLine = Key(Vec(Bits(Address.btbMetaWidth bits), BTB.NUM_WAYS))
-  val btbDataLine = Key(Vec(Bits(30 bits), BTB.NUM_WAYS))
-  val btbHitLine  = Key(Bits(BTB.NUM_WAYS bits))
-  val btbHit      = Key(Bool())
-  val btbData     = Key(UInt(30 bits))
-  val btbPC_2     = Key(Bool())
-  val btbP        = Key(Bits(BTB.NUM_WAYS - 1 bits))
+  val btbHitLine = Key(Bits(BTB.NUM_WAYS bits))
+  val btbHit     = Key(Bool())
+  val btbPC_2    = Key(Bool())
+  val btbOH      = Key(Bits(BTB.NUM_WAYS bits))
+  val btbMeta    = Key(Bits(Address.btbMetaWidth bits))
+  val btbData    = Key(Bits(30 bits))
+  val btbP       = Key(Bits(BTB.NUM_WAYS - 1 bits))
 
   val bhtI  = Key(UInt(BHT.INDEX_WIDTH bits))
   val bhtV  = Key(Bits(BHT.DATA_WIDTH bits))
@@ -50,89 +50,103 @@ class BranchPredictor(numStages: Int) extends Area {
   val phtVP = Key(UInt(2 bits))
   val phtVM = Key(UInt(2 bits))
 
-  val io = new Bundle {
-    val pc     = Address()
-    val nextPC = Address()
-
-    val isDJump1 = Bool()
-    val jumpPC1  = UInt(32 bits)
-
-    val isDJump2 = Bool()
-    val jumpPC2  = UInt(32 bits)
-
-    val jump = Bool()
-
-    val assignJump = Bits(2 bits)
-    val jumpPC     = UInt(32 bits)
-  }
-
-  val btb = new Cache(BTB.NUM_ENTRIES, BTB.NUM_WAYS, BTB.INDEX_WIDTH, Address.btbMetaWidth, 30)
+  val btb = new Cache(BTB.NUM_ENTRIES / BTB.NUM_WAYS, BTB.NUM_WAYS, BTB.INDEX_WIDTH, Address.btbMetaWidth, 30)
   val bpu = new BPU
 
-  val write1 = new ComponentStage {
+  val write2 = new ComponentStage {
+    val io = new Bundle {
+      val assignJump = Bool()
+    }
+    /**/
     btb.io.w.en := stored(btbHit) | stored(isDJump)
-    btb.io.w.pEn := btb.io.w.en
+    btb.io.w.oh := stored(btbOH)
     btb.io.w.index := stored(pc).btbIndex
-    btb.io.w.metaLine := stored(btbMetaLine)
-    btb.io.w.dataLine := stored(btbDataLine)
+    btb.io.w.meta := stored(btbMeta)
+    btb.io.w.data := stored(btbData)
     btb.io.w.p := stored(btbP)
     /**/
     bpu.io.w.bhtEn := stored(isDJump)
     bpu.io.w.phtEn := stored(isDJump)
     bpu.io.w.bhtI := stored(bhtI)
-    bpu.io.w.bhtV := stored(bhtV)(1, BHT.DATA_WIDTH - 1 bits) ## io.jump
+    bpu.io.w.bhtV := stored(bhtV)(1, BHT.DATA_WIDTH - 1 bits) ## io.assignJump
     bpu.io.w.phtI := stored(phtI)
-    bpu.io.w.phtV := io.jump ? stored(phtVP) | stored(phtVM)
+    bpu.io.w.phtV := io.assignJump ? stored(phtVP) | stored(phtVM)
   }
 
-  val calc1 = new ComponentStage {
+  val write1 = new ComponentStage {
+    val io = new Bundle {
+      val pc         = Address()
+      val isDJump1   = Bool()
+      val jumpPC1    = UInt(32 bits)
+      val isDJump2   = Bool()
+      val jumpPC2    = UInt(32 bits)
+      val btbHit     = Bool()
+      val btbHitLine = Bits(BTB.NUM_WAYS bits)
+      val btbP       = Bits(BTB.NUM_WAYS - 1 bits)
+      val bhtV       = Bits(BHT.DATA_WIDTH bits)
+      val phtV       = UInt(2 bits)
+    }
+    /**/
     val pc_2  = io.isDJump2
     val valid = io.isDJump1 | io.isDJump2
-    val we    = stored(btbHit) ? stored(btbHitLine) | btb.plru(stored(btbP))
+    val we    = io.btbHit ? io.btbHitLine | btb.plru(io.btbP)
+    output(pc) := io.pc
     output(isDJump) := valid
-    output(btbMetaLine) := stored(btbMetaLine)
-    output(btbDataLine) := stored(btbDataLine)
-    output(btbMetaLine).oneHotAccess(we) := pc_2 ## valid ## stored(pc).btbTag
-    output(btbDataLine).oneHotAccess(we) := B(pc_2 ? io.jumpPC2 | io.jumpPC1)
-    btb.getP(stored(btbP), we, valid, output(btbP))
+    output(btbHit) := io.btbHit
+    output(btbOH) := we
+    output(btbMeta) := pc_2 ## valid ## io.pc.btbTag
+    output(btbData) := B(pc_2 ? io.jumpPC2 | io.jumpPC1)(2, 30 bits)
+    btb.getP(io.btbP, we, valid, output(btbP))
     /**/
-    output(phtVP) := U((stored(phtV)(1) | stored(phtV)(0)) ## (stored(phtV)(1) | !stored(phtV)(0)))
-    output(phtVM) := U((stored(phtV)(1) & stored(phtV)(0)) ## (stored(phtV)(1) & !stored(phtV)(0)))
+    output(bhtI) := io.pc.bhtBase ^ io.pc.bhtIndex
+    output(bhtV) := io.bhtV
+    output(phtI) := U(io.bhtV, PHT.INDEX_WIDTH bits) ^ io.pc.phtIndex
+    output(phtVP) := U((io.phtV(1) | io.phtV(0)) ## (io.phtV(1) | !io.phtV(0)))
+    output(phtVM) := U((io.phtV(1) & io.phtV(0)) ## (io.phtV(1) & !io.phtV(0)))
   }
 
   val read2 = new ComponentStage {
+    val io = new Bundle {
+      val assignJump = Bits(2 bits)
+      val jumpPC     = UInt(32 bits)
+    }
+    /**/
     bpu.io.r.phtI := stored(phtI)
     output(phtV) := bpu.io.r.phtV
     /**/
     val j = stored(btbHit) & output(phtV)(1)
     io.assignJump := (j & stored(btbPC_2)) ## (j & !stored(btbPC_2))
-    io.jumpPC := stored(btbData) @@ U"00"
+    io.jumpPC := U(stored(btbData) ## B"00")
   }
 
   val read1 = new ComponentStage {
+    val io = new Bundle {
+      val nextPC = Address()
+      val pc     = Address()
+    }
+    /**/
     btb.io.r.en := will.input
     btb.io.r.index := io.nextPC.btbIndex
-    output(btbMetaLine) := btb.io.r.metaLine
-    output(btbDataLine) := btb.io.r.dataLine
+    val metaLine = btb.io.r.metaLine
+    val dataLine = btb.io.r.dataLine
     for (i <- 0 to 3) {
-      output(btbHitLine)(i) := B"1" ## io.pc.btbTag === output(btbMetaLine)(i)(0, 1 + Address.btbTagWidth bits)
+      output(btbHitLine)(i) := (B"1" ## io.pc.btbTag) === metaLine(i)(0, 1 + Address.btbTagWidth bits)
     }
     output(btbHit) := output(btbHitLine).orR
-    output(btbData) := U(output(btbDataLine).oneHotAccess(output(btbHitLine)))
-    output(btbPC_2) := output(btbMetaLine).oneHotAccess(output(btbHitLine))(Address.btbMetaWidth - 1)
+    output(btbData) := dataLine.oneHotAccess(output(btbHitLine))
+    output(btbPC_2) := metaLine.oneHotAccess(output(btbHitLine))(Address.btbMetaWidth - 1)
     /**/
     bpu.io.r.bhtI := io.pc.bhtBase ^ io.pc.bhtIndex
-    output(bhtI) := bpu.io.r.bhtI
     output(bhtV) := bpu.io.r.bhtV
     output(phtI) := U(output(bhtV), PHT.INDEX_WIDTH bits) ^ io.pc.phtIndex
     /**/
     output(pc) := io.pc
   }
 
+  write2.interConnect()
+  write1.send(write2)
   write1.interConnect()
-  calc1.send(write1)
-  calc1.interConnect()
-  read2.send(calc1)
+
   read2.interConnect()
   read1.send(read2)
   read1.interConnect()
@@ -141,7 +155,7 @@ class BranchPredictor(numStages: Int) extends Area {
 object BranchPredictor {
   def main(args: Array[String]): Unit = {
     SpinalVerilog(new Component {
-      val bp = new BranchPredictor(5)
+      val bp = new BranchPredictor()
     })
   }
 }
--- src/main/scala/ip/Cache.scala
+++ src/main/scala/ip/Cache.scala
@@ -2,27 +2,27 @@ package ip
 
 import spinal.core._
 
-class SDPRAM(numEntries: Int, numWays: Int, indexWidth: Int, entryWidth: Int) extends Component {
+class SDPRAM(numEntries: Int, indexWidth: Int, entryWidth: Int) extends Component {
   val memGeneric = new xpm_memory_sdpram_generic
   memGeneric.ADDR_WIDTH_A = indexWidth
   memGeneric.ADDR_WIDTH_B = indexWidth
-  memGeneric.BYTE_WRITE_WIDTH_A = numWays * entryWidth
+  memGeneric.BYTE_WRITE_WIDTH_A = entryWidth
   memGeneric.MEMORY_PRIMITIVE = "block"
   memGeneric.MEMORY_SIZE = numEntries * entryWidth
-  memGeneric.READ_DATA_WIDTH_B = numWays * entryWidth
+  memGeneric.READ_DATA_WIDTH_B = entryWidth
   memGeneric.READ_LATENCY_B = 1
-  memGeneric.WRITE_DATA_WIDTH_A = numWays * entryWidth
+  memGeneric.WRITE_DATA_WIDTH_A = entryWidth
   memGeneric.WRITE_MODE_B = "write_first"
   val mem = new xpm_memory_sdpram(memGeneric)
 
   val io = new Bundle {
     val ena   = in Bool ()
     val addra = in UInt (indexWidth bits)
-    val dina  = in Vec (Bits(entryWidth bits), numWays)
+    val dina  = in Bits (entryWidth bits)
 
     val enb   = in Bool ()
     val addrb = in UInt (indexWidth bits)
-    val doutb = out Vec (Bits(entryWidth bits), numWays)
+    val doutb = out Bits (entryWidth bits)
   }
 
   val prevDina  = RegNext(mem.io.dina)
@@ -39,58 +39,52 @@ class SDPRAM(numEntries: Int, numWays: Int, indexWidth: Int, entryWidth: Int) ex
   io.doutb.assignFromBits(conflict ? prevDina | mem.io.doutb)
 }
 
-class Cache(
-    numEntries: Int,
-    numWays: Int,
-    indexWidth: Int,
-    metaWidth: Int,
-    dataWidth: Int
-) extends Component {
-  val dataMem = new SDPRAM(numEntries, numWays, indexWidth, dataWidth)
-  val metaMem = new SDPRAM(numEntries, numWays, indexWidth, metaWidth)
-  val pMem    = Mem(Bits(numWays - 1 bits), numEntries / numWays) randBoot
+class Cache(entriesPerWay: Int, numWays: Int, indexWidth: Int, metaWidth: Int, dataWidth: Int) extends Component {
+  val metaMem = Seq.fill(numWays)(new SDPRAM(entriesPerWay, indexWidth, metaWidth))
+  val dataMem = Seq.fill(numWays)(new SDPRAM(entriesPerWay, indexWidth, dataWidth))
+  val pMem    = Mem(Bits(numWays - 1 bits), entriesPerWay).randBoot()
 
   val io = new Bundle {
     val w = new Bundle {
-      val en       = in Bool ()
-      val index    = in UInt (indexWidth bits)
-      val dataLine = in Vec (Bits(dataWidth bits), numWays)
-      val metaLine = in Vec (Bits(metaWidth bits), numWays)
-
-      val pEn = in Bool ()
-      val p   = in Bits (numWays - 1 bits)
+      val en    = in Bool ()
+      val oh    = in Bits (numWays bits)
+      val index = in UInt (indexWidth bits)
+      val data  = in Bits (dataWidth bits)
+      val meta  = in Bits (metaWidth bits)
+      val p     = in Bits (numWays - 1 bits)
     }
     val r = new Bundle {
       val en       = in Bool ()
       val index    = in UInt (indexWidth bits)
       val dataLine = out Vec (Bits(dataWidth bits), numWays)
       val metaLine = out Vec (Bits(metaWidth bits), numWays)
-
-      val p = out Bits (numWays - 1 bits)
+      val p        = out Bits (numWays - 1 bits)
     }
   }
 
-  //
-  dataMem.io.ena := io.w.en
-  dataMem.io.addra := io.w.index
-  dataMem.io.dina := io.w.dataLine
+  for (i <- 0 until numWays) {
+    val we = io.w.en & io.w.oh(i)
+    /**/
+    dataMem(i).io.ena := we
+    dataMem(i).io.addra := io.w.index
+    dataMem(i).io.dina := io.w.data
 
-  dataMem.io.enb := io.r.en
-  dataMem.io.addrb := io.r.index
-  io.r.dataLine := dataMem.io.doutb
+    dataMem(i).io.enb := io.r.en
+    dataMem(i).io.addrb := io.r.index
+    io.r.dataLine(i) := dataMem(i).io.doutb
+    /**/
+    metaMem(i).io.ena := we
+    metaMem(i).io.addra := io.w.index
+    metaMem(i).io.dina := io.w.meta
 
-  //
-  metaMem.io.ena := io.w.en
-  metaMem.io.addra := io.w.index
-  metaMem.io.dina := io.w.metaLine
-
-  metaMem.io.enb := io.r.en
-  metaMem.io.addrb := io.r.index
-  io.r.metaLine := metaMem.io.doutb
+    metaMem(i).io.enb := io.r.en
+    metaMem(i).io.addrb := io.r.index
+    io.r.metaLine(i) := metaMem(i).io.doutb
+  }
 
   //
   io.r.p := pMem(io.r.index)
-  when(io.w.pEn) { pMem(io.w.index) := io.w.p }
+  when(io.w.en) { pMem(io.w.index) := io.w.p }
 
   //
   def plru(p: Bits, i: Int = 1, v: Bool = True): Bits = {
@@ -123,6 +117,6 @@ class Cache(
 
 object Cache {
   def main(args: Array[String]): Unit = {
-    SpinalVerilog(new Cache(4096, 4, 10, 10, 10))
+    SpinalVerilog(new Cache(1024, 4, 10, 10, 10))
   }
 }
